3e9bebe48b195e7682f9524d755924985b910835
Humza920 <humza.rehman.779@gmail.com>
2024-02-29 08:09:28 -0800

    DONE

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 // IF AND ELSE STATMENTS
 
+// QUESTION 1(answer)
+// var number = (+prompt("ENTER YOU NUMBER"))
+// if (number%3===0) {console.log("Number is divisible by 3");
+// }
+// else{console.log("Number is not divisible by 3");}
+
 // QUESTION 2(answer)
-var number1 = (+prompt("ENTER TO KNOW ITS EVEN OR ODD NUMBER"));
-if (number1%2===0) {console.log("it's a even number");
-} else {console.log("it's not a even number");
-}
+// var number1 = (+prompt("ENTER TO KNOW ITS EVEN OR ODD NUMBER"));
+// if (number1%2===0) {console.log("it's a even number");
+// } else {console.log("it's not a even number");
+// }
 
 // QUESTION 3(answer)
-var age = (+prompt("ENTER YOU AGE"))
-if (age<=18) {console.log("TOO YOUNG");
-} else {console.log("OLD ENOUGH");
-}
+// var age = (+prompt("ENTER YOU AGE"))
+// if (age<=18) {console.log("TOO YOUNG");
+// } else {console.log("OLD ENOUGH");
+// }
 
 // QUESTION 4(answer)
-var you =(prompt("ENTER YOUR NAME"))
-if ( you==="Humza" ) {console.log("WELCOME NASEEM HAPPY CODE");
-}
+// var you =(prompt("ENTER YOUR NAME"))
+// if ( you==="Humza" ) {console.log("WELCOME HUMZA HAPPY CODE");
+// }
 
 // QUESTION 10(answer)
+// var password = ("sweat")
+// var pass = (prompt("ENTER YOUR PASSWORD"))
+// if (pass===password) {console.log("Correct! The password you entered matches the original password");
+// }else{console.log("Incorrect passsword");}
+
+// QUESTION 9(answer)
+// let calendar = (2000) 
+// let me = (+prompt("FIND A LEAP YEAR"))
+// if (me===calendar) {console.log("ITS A LEAP YEAR");    
+// } else {console.log("IT'S NOT A LEAP YEAR");
+// }
+
+// QUESTION 11(answer)
+// var firstName = (prompt ("ENTER YOR USER NAME"));
+
+//  if (firstName==="Fahad") {
+//     document.write("Hello Fahad!");
+//    }
+// else if (firstName==="Ali") {
+//     document.write("Hello Ali!");
+//    }
+// else{document.write("YOU ARE NOT FAHAD NOR ALI")}   
+
+// QUESTION 12(answer)
+// var hour = (+prompt("ENTER HOUR"))
+// if (hour<=13) {console.log("GOOG DAY !");
+// } else {console.log("GOOD EVENING !");
+// }
+
+// QUESTION 15(answer)
+// var currentHour = (+prompt("What the current hour is"))
+// if (currentHour>=6 && currentHour<=9) {console.log("Breakfast is served.");
+// }
+// else if (currentHour>=11 && currentHour<=13) {console.log("Time for lunch.");}
+// else if (currentHour>=17 && currentHour<=20) {console.log("It's dinner time.");}
+// else{console.log("Sorry, you'll have to wait, or go get a snack.");}
+
+// QUESTION 15(answer)
