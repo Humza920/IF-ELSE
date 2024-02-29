bf4ced9c4a019f3e7ea7c6a63992fb5eb8bfcf75
Humza920 <humza.rehman.779@gmail.com>
2024-02-29 01:51:57 -0800

    IF/ELSE

---
+++ app.js
@@ -0,0 +1,20 @@
+// IF AND ELSE STATMENTS
+
+// QUESTION 2(answer)
+var number1 = (+prompt("ENTER TO KNOW ITS EVEN OR ODD NUMBER"));
+if (number1%2===0) {console.log("it's a even number");
+} else {console.log("it's not a even number");
+}
+
+// QUESTION 3(answer)
+var age = (+prompt("ENTER YOU AGE"))
+if (age<=18) {console.log("TOO YOUNG");
+} else {console.log("OLD ENOUGH");
+}
+
+// QUESTION 4(answer)
+var you =(prompt("ENTER YOUR NAME"))
+if ( you==="Humza" ) {console.log("WELCOME NASEEM HAPPY CODE");
+}
+
+// QUESTION 10(answer)
