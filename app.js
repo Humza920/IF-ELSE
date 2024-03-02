c720a9b3e1e761d4b047af71672af2e4623f84b8
Humza920 <humza.rehman.779@gmail.com>
2024-03-02 03:40:48 -0800

    LITTLE

--- app.js
+++ app.js
@@ -61,11 +61,60 @@
 // else if (currentHour>=17 && currentHour<=20) {console.log("It's dinner time.");}
 // else{console.log("Sorry, you'll have to wait, or go get a snack.");}
 
-// QUESTION 15(answer)
-var time = (+prompt("ENTER A VALID TIME"))
-if (time>=0 && time<1200 ) {console.log("GOOD MORNING !");
-}
-else if (time>=1200 && time<1700) {console.log("GOOD AFTERNOON !");}
-else if (time>=1700 && time<2100) {console.log("GOOD EVENING !");}
-else if (time>=2100 && time<2359) {console.log("GOOD NIGHT !");}
-else {console.log("DO IT CORRECT");}
+// QUESTION 8(answer)
+// var time = (+prompt("ENTER A VALID TIME"))
+// if (time>=0 && time<1200 ) {console.log("GOOD MORNING !");
+// }
+// else if (time>=1200 && time<1700) {console.log("GOOD AFTERNOON !");}
+// else if (time>=1700 && time<2100) {console.log("GOOD EVENING !");}
+// else if (time>=2100 && time<2359) {console.log("GOOD NIGHT !");}
+// else {console.log("DO IT CORRECT");}
+
+// QUESTION 7(answer)
+// var num1 = (+prompt("ENTER NUM 1"))
+// var num2 = (+prompt("ENTER NUM 2"))
+// console.log(num1+num2-num1*num2/num1);
+// console.log(num1%num2);
+
+// QUESTION 13(answer)
+// var integer1 = (+prompt("ËNTER IT"))
+// var integer2 = (+prompt("ËNTER IT"))
+// if (integer1>integer2) {console.log(integer1);
+// } 
+// else if (integer2>integer1) {console.log(integer2);}
+// else {console.log("PUT THE INTEGER");   
+// }
+
+// QUESTION 16(answer)
+// var string = ("Humza")
+// var bolean = (true)
+// var number = (27)
+// var wwe = 
+// console.log(typeof string);
+// console.log(typeof bolean);
+// console.log(typeof number);
+// console.log(typeof wwe);
+
+// QUESTION 14(answer)
+// var input = (+prompt("INPUT A NUMBER"))
+// if (input>0) {console.log("IT IS POSITIVE");
+// }
+// else if (input<0) {console.log("IT IS NEGATIVE");}
+// else if (input===0) {console.log("IT IS NEUTRAL");}
+// else {console.log("YOU DO NOT ENTER A NUMBER");}
+
+// QUESTION 19(answer)
+// var month = (+prompt("ENTER A NUMBER OF MONTH"))
+// if (month===1) {alert("ITS JAMUARY");}
+// else if (month===2) {alert("ITS FEBURARY");}
+// else if (month===3) {alert("ITS MARCH");}
+// else if (month===4) {alert("ITS APRIL");}
+// else if (month===5) {alert("ITS MAY");}
+// else if (month===6) {alert("ITS JUNE");}
+// else if (month===7) {alert("ITS JULY");}
+// else if (month===8) {alert("ITS AUGUST");}
+// else if (month===9) {alert("ITS SEPTEMBER");}
+// else if (month===10) {alert("ITS OCTOBER");}
+// else if (month===11) {alert("ITS NOVEMBER");}
+// else if (month===12) {alert("ITS DECEMBER");}
+// else {alert("NOT PRESENT");}
